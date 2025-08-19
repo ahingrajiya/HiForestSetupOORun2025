5b0cf0f7d4dfc38f12d59e1ce7c4c8df45155e01
Abhishek Hingrajiya <abhi.h123@gmail.com>
2025-08-19 14:51:51 -0500

    typo correction

--- forest_CMSSWConfig_Run3_OXY_MC_miniAOD.py
+++ forest_CMSSWConfig_Run3_OXY_MC_miniAOD.py
@@ -148,7 +148,7 @@ if addR4Jets :
         # Recluster using an alias "0" in order not to get mixed up with the default AK4 collections
         process.jetsR4 = cms.Sequence()
         jetName = 'akCs0PF'
-        setupHeavyIonJets(jetName, process.jetsR4, process, isMC = 1, radius = 0.40, JECTag = 'AK4PF', doFlow = False, matchJets = matchJets, doWTA= True)
+        setupHeavyIonJets(jetName, process.jetsR4, process, isMC = 1, radius = 0.40, JECTag = 'AK4PF', doFlow = False, matchJets = True, doWTA= True)
         process.akCs0PFpatJetCorrFactors.levels = ['L2Relative', 'L3Absolute']
         process.akCs4PFJetAnalyzer.jetTag = jetName + 'patJets'
         process.akCs4PFJetAnalyzer.jetName = jetName
